1add2bd4796473d37461644034a759b9ea2f6819
Bekzod1999 <developmentfirm1999@gmail.com>
2022-08-26 18:16:26 +0500

    010

--- List10.py
+++ List10.py
@@ -1,3 +1,26 @@
+# def main(list_num):
+#     """
+#     A list of numbers consisting of several elements is given. Return the largest between the first and last elements.
+#     Args:
+#         list_num (list): parameter
+#     Returns:
+#         int: return answer
+#     """
+#     largestNumber = 0
+#     i = 0
+#     k = list_num[0]
+#     while i < len(list_num):
+        
+#         if list_num[i] > k:
+#             k = list_num[i]
+#         i += 1
+
+#     return k
+# x = main([-5, 0, -1, -4, -30])
+# print(x)
+
+
+
 def main(list_num):
     """
     A list of numbers consisting of several elements is given. Return the largest between the first and last elements.
@@ -7,14 +30,10 @@ def main(list_num):
         int: return answer
     """
     largestNumber = 0
-    i = 0
-    k = list_num[0]
-    while i < len(list_num):
-        
-        if list_num[i] > k:
-            k = list_num[i]
-        i += 1
-
-    return k
-x = main([-5, 0, -1, -4, -30])
+            
+    if list_num[0] > list_num[-1]:
+        return list_num[0]
+    else:
+        return list_num[-1]
+x = main([12, 2, 5, 2, 7, 9, 100])
 print(x)
